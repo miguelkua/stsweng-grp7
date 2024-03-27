e8ba8deb769acc0e5876a85139db4a593e6c715e
Retraxi <raphael_tanai@dlsu.edu.ph>
2024-03-27 21:14:02 +0800

    Simplest Logout case
    checks if logout was successful via status

--- tests/logout.test.js
+++ tests/logout.test.js
@@ -18,10 +18,22 @@ app.use(session({ secret: 'testsecret', resave: false, saveUninitialized: true }
 app.use('/logout', logoutRoute);
 
 describe('GET /logout', () => {
-    test('responds with login page', async () => {
+    test('Goes back to the login apgee', async () => {
       const response = await request(app).get('/logout');
-      expect(response.status).toBe(200);
-      console.log("response.text", response.text);
       console.log(response);
+      
+      expect(response.statusCode).toBe(200)
+      expect(response.session).toBeFalsy();
+      console.log("response.text", response.text);
+      
     });
+
+    /*
+    test('In case of error, receive a statuscode of 500', (req) => {
+
+      const response = request(app).get('/logout');
+
+      expect(response.statusCode).toBe(500);
+      expect(response.text).toContain('Internal');
+    })*/
 });
